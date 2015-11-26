df650aa1fd375991123835f06d5f974f59cd1757
David Mendez <desiratech@gmail.com>
2015-11-26 01:05:35 -0600

    basic setup and installed knex, hapi, sqlite and socketio

---
+++ src/typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="socket.io/socket.io.d.ts" />
+/// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="hapi/hapi.d.ts" />
+/// <reference path="knex/knex.d.ts" />
+/// <reference path="sqlite3/sqlite3.d.ts" />
